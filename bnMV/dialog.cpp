ca46d6e4e133bc1982902f660e350436fe2a00ff
bnoden <brandroid369@gmail.com>
2017-03-21 20:36:57 -0500

    .

--- bnMV/dialog.cpp
+++ bnMV/dialog.cpp
@@ -6,9 +6,53 @@ Dialog::Dialog(QWidget *parent) :
     ui(new Ui::Dialog)
 {
     ui->setupUi(this);
+
+    model = new QStringListModel(this);
+
+    QStringList bnList;
+    bnList << "STM" << "AVR" << "TMC4123" << "PIC";
+    std::sort(bnList.begin(), bnList.end());
+
+    model->setStringList(bnList);
+    ui->listView->setModel(model);
+    ui->comboBox->setModel(model);
+
+    ui->listView->setEditTriggers(
+        QAbstractItemView::AnyKeyPressed | QAbstractItemView::DoubleClicked
+                );
+
 }
 
 Dialog::~Dialog()
 {
     delete ui;
+}
+void Dialog::on_btnAdd_clicked()
+{
+    quint8 row = model->rowCount();
+    model->insertRows(row, 1);
+    QModelIndex index = model->index(row);
+    ui->listView->setCurrentIndex(index);
+    ui->listView->edit(index);
+
+}
+
+void Dialog::on_btnInsert_clicked()
+{
+    quint8 row = ui->listView->currentIndex().row();
+    model->insertRows(row, 1);
+    QModelIndex index = model->index(row);
+    ui->listView->setCurrentIndex(index);
+    ui->listView->edit(index);
+}
+
+void Dialog::on_btnDelete_clicked()
+{
+    model->removeRows(ui->listView->currentIndex().row(),1);
+}
+
+void Dialog::on_btnSort_clicked()
+{
+    quint8 row = model->rowCount();
+    model->sort(row);
 }
--- bnMV/dialog.h
+++ bnMV/dialog.h
@@ -1,6 +1,8 @@
 #ifndef DIALOG_H
 #define DIALOG_H
 
+#include <QtCore>
+#include <QtGui>
 #include <QDialog>
 
 namespace Ui {
@@ -15,8 +17,18 @@ public:
     explicit Dialog(QWidget *parent = 0);
     ~Dialog();
 
+private slots:
+    void on_btnAdd_clicked();
+
+    void on_btnInsert_clicked();
+
+    void on_btnDelete_clicked();
+
+    void on_btnSort_clicked();
+
 private:
     Ui::Dialog *ui;
+    QStringListModel *model;
 };
 
 #endif // DIALOG_H
