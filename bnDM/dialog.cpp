a5a715568722dec5c53e407e6fdb89945bf1cbfb
bnoden <brandroid369@gmail.com>
2017-03-23 17:03:01 -0500

    .

--- bnDM/dialog.cpp
+++ bnDM/dialog.cpp
@@ -1,5 +1,6 @@
 #include "dialog.h"
 #include "ui_dialog.h"
+#include <QInputDialog>
 
 Dialog::Dialog(QWidget *parent) :
     QDialog(parent),
@@ -9,11 +10,19 @@ Dialog::Dialog(QWidget *parent) :
 
     model = new QDirModel(this);
     model->setReadOnly(false);
+    model->setSorting(QDir::DirsFirst | QDir::IgnoreCase | QDir::Name);
 
     ui->treeView->setModel(model);
 
-    model->sort(0);
+    QModelIndex index = model->index("C:/");
+
+    ui->treeView->expand(index);
+    ui->treeView->scrollTo(index);
+    ui->treeView->setCurrentIndex(index);
+    ui->treeView->resizeColumnToContents(0);
+
 }
+
 
 Dialog::~Dialog()
 {
@@ -24,12 +33,25 @@ Dialog::~Dialog()
 
 void Dialog::on_btnMkdir_clicked()
 {
+    QModelIndex index = ui->treeView->currentIndex();
+    if (!index.isValid()) { return; }
 
+    QString name = QInputDialog::getText(this, "New", "Create new directory");
+    if (name.isEmpty()) { return; }
+    model->mkdir(index, name);
 }
 
 void Dialog::on_btnRmdir_clicked()
 {
+    QModelIndex index = ui->treeView->currentIndex();
+    if (!index.isValid()) { return; }
 
+    if (model->fileInfo(index).isDir()) {
+        model->rmdir(index);
+    }
+    else {
+        model->remove(index);
+    }
 }
 
 void Dialog::on_btnCancel_clicked()
